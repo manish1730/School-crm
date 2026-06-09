393b996432e3374a44bb585fe6638d47c6c414b4
Vanshika <vanshika3214@gmail.com>
2026-06-09 10:14:34 +0530

    DAta

---
+++ Server/routes/admissionRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+import {
+  createAdmission
+}
+from "../controllers/admissionController.js";
+
+const router = express.Router();
+
+router.post(
+  "/create",
+  createAdmission
+);
+
+export default router;
--- Server/server.js
+++ Server/server.js
@@ -1,21 +1,61 @@
+// import express from "express";
+// import authRoutes from "./routes/authRoutes.js";
+
+// const app = express();
+
+// // Middleware
+// app.use(express.json());
+
+// // Routes
+// app.use("/api/auth", authRoutes);
+
+// // Test Route
+// app.get("/", (req, res) => {
+//   res.send("School CRM Backend Running");
+// });
+
+// const PORT = 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
 import authRoutes from "./routes/authRoutes.js";
+import admissionRoutes from "./routes/admissionRoutes.js";
+
+dotenv.config();
 
 const app = express();
 
-// Middleware
+app.use(cors());
+
 app.use(express.json());
 
-// Routes
+mongoose
+.connect(process.env.MONGO_URI)
+.then(()=>{
+   console.log("MongoDB Connected");
+})
+.catch(err=>{
+   console.log(err);
+});
+
 app.use("/api/auth", authRoutes);
 
-// Test Route
-app.get("/", (req, res) => {
+app.use(
+  "/api/admission",
+  admissionRoutes
+);
+
+app.get("/",(req,res)=>{
   res.send("School CRM Backend Running");
 });
 
-const PORT = 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(5000,()=>{
+  console.log("Server running on port 5000");
 });
